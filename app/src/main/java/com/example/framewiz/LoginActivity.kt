d7801dfd30615894144b05f6819710e145c7af98
Daneeels <sasongkoindra76@gmail.com>
2023-06-04 15:17:04 +0700

    [ADD] Adding login feature

--- app/src/main/java/com/example/framewiz/LoginActivity.kt
+++ app/src/main/java/com/example/framewiz/LoginActivity.kt
@@ -11,17 +11,17 @@ import android.util.Patterns
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
-import com.example.framewiz.databinding.ActivityRegisterBinding
+import com.example.framewiz.databinding.ActivityLoginBinding
 
-class RegisterActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_login)
 
-        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setupView()
@@ -43,56 +43,8 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun setUpAction() {
 
-        var nameValid = false
         var emailValid = false
         var passwordValid = false
-
-        binding.nameEdt.addTextChangedListener(
-            object : TextWatcher {
-                override fun beforeTextChanged(
-                    s: CharSequence,
-                    start: Int,
-                    count: Int,
-                    after: Int
-                ) {
-                }
-
-                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                    if (s.isEmpty()) {
-                        binding.nameEdtLayout.error = " "
-                        binding.nameEdtLayout.setErrorIconOnClickListener {
-                            Toast.makeText(
-                                applicationContext,
-                                "This field is required",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    } else {
-                        binding.nameEdtLayout.error = null
-                        nameValid = true
-                        Log.e("name", nameValid.toString())
-                    }
-                }
-
-                override fun afterTextChanged(s: Editable) {
-                    nameValid = false
-                    if (s.isEmpty()) {
-                        binding.nameEdtLayout.error = " "
-                        binding.nameEdtLayout.setErrorIconOnClickListener {
-                            Toast.makeText(
-                                applicationContext,
-                                "This field is required",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    } else {
-                        binding.nameEdtLayout.error = null
-                        nameValid = true
-                        Log.e("name", nameValid.toString())
-                    }
-                }
-            }
-        )
 
         binding.emailEdt.addTextChangedListener(
             object : TextWatcher {
@@ -238,8 +190,8 @@ class RegisterActivity : AppCompatActivity() {
             }
         )
 
-        binding.registerBtn.setOnClickListener {
-            if (nameValid && emailValid && passwordValid) {
+        binding.loginBtn.setOnClickListener {
+            if (emailValid && passwordValid) {
                 Toast.makeText(applicationContext, "Amang sam", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(applicationContext, "Belum amang sam", Toast.LENGTH_SHORT).show()
@@ -252,5 +204,4 @@ class RegisterActivity : AppCompatActivity() {
     private fun isValidEmail(target: CharSequence): Boolean {
         return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches()
     }
-
 }
